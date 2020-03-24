cad8441166e9bb3afa0e18cf206ed0094531eb3c
Ajeigbe John Oluwaseyi <slimmjohn4bright@gmail.com>
2020-03-24 11:44:25 +0100

    Footer all done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
-import React from 'react';
-import './App.css';
-import { Btn } from './elements/Button/Button'
+import React from "react";
+import Footer from "./elements/Footer";
 
 function App() {
   return (
     <div className="App">
-      <h1 class="display-4"> Hello Ninja <span role="img" aria-label="smiling-face">😸</span> , Start contributing... <span role="img" arial-label="keyboard"> ⌨ </span> </h1>
-      <Btn
-      color="secondary"
-      type="submit"
-      name="example"
-      >
-        ❣
-      </Btn>
+      <Footer />
     </div>
   );
 }
---
+++ src/elements/Footer/index.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebookF,
+  faTwitter,
+  faInstagram
+} from "@fortawesome/free-brands-svg-icons";
+import { faChevronRight, faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import "./footer.styles.scss";
+
+const Footer = () => (
+  <footer className="footer">
+    <div className="container">
+      <h1 className="footer__brand text-center">Feeling Pressed</h1>
+
+      <div className="row">
+        <div className="col-lg-8 offset-lg-2 col-sm-12">
+          <p className="footer- mb-5 text-center">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis
+            tenetur quas iure doloremque? Qui illum deserunt, nostrum,
+            doloremque quaerat obcaecati unde ut nihil itaque commodi blanditiis
+            non minima neque harum.
+          </p>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-lg-6 offset-lg-3 col-sm-12">
+          <section className="footer__form-wrap">
+            <h6 className="form-heading text-center">Subscribe</h6>
+            <form action="" className="footer-form">
+              <div className="input-group">
+                <input
+                  type="text"
+                  placeholder="Email..."
+                  className="form-input"
+                />{" "}
+                <button className="btn-submit">
+                  {" "}
+                  <FontAwesomeIcon icon={faChevronRight} />
+                </button>
+              </div>
+            </form>
+          </section>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-lg-6 offset-lg-3 col-sm-12">
+          <section className="social-media">
+            <div className="media-handles mt">
+              <FontAwesomeIcon icon={faFacebookF} className="icon facebook" />
+            </div>
+            <div className="media-handles mt">
+              <FontAwesomeIcon icon={faTwitter} className="icon twitter" />
+            </div>
+
+            <div className="media-handles mt">
+              <FontAwesomeIcon icon={faEnvelope} className="icon envelope" />
+            </div>
+            <div className="media-handles">
+              <FontAwesomeIcon icon={faInstagram} className="icon instagram" />
+            </div>
+          </section>
+        </div>
+      </div>
+      <p className="copyright text-center">
+        2020 Copyright &copy; Feeling Pressed International
+      </p>
+    </div>
+  </footer>
+);
+export default Footer;
