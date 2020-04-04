15fd19fe3463d86c163de9ecf40ba19dd1545e03
Ajeigbe John Oluwaseyi <slimmjohn4bright@gmail.com>
2020-04-04 13:27:44 -0400

    I create an About directory

---
+++ src/pages/About/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./about.styles.scss";
+
+const AboutPage = () => (
+  <main className="about-page">
+    <div className="container"></div>
+  </main>
+);
+
+export default AboutPage;
