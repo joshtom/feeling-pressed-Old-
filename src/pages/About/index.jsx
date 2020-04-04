a9a1f03bcd5548e7cbd8f23abb5885060df772cb
Ajeigbe John Oluwaseyi <slimmjohn4bright@gmail.com>
2020-04-04 13:35:58 -0400

    About page in progress

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,10 +1,40 @@
 import React from "react";
+import Header from "../../components/header";
+import Footer from "../../components/Footer";
+
 import "./about.styles.scss";
 
 const AboutPage = () => (
-  <main className="about-page">
-    <div className="container"></div>
-  </main>
+  <>
+    <Header />
+    <main className="about-page">
+      <section className="people-bg">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-4 offset-lg-4 col-sm-12 ">
+              <h2 className="h2 text-center">About Us</h2>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="mission">
+        <div className="container">
+          <div className="row"></div>
+        </div>
+      </section>
+      <section className="vision">
+        <div className="container">
+          <div className="row"></div>
+        </div>
+      </section>
+      <section className="goal">
+        <div className="container">
+          <div className="row"></div>
+        </div>
+      </section>
+    </main>
+    <Footer />
+  </>
 );
 
 export default AboutPage;
