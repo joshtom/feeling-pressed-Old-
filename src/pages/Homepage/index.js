42a45ee7d0a87512237cd63c1207551246070699
joshtom <olajidejoshua4real@gmail.com>
2020-04-10 21:15:56 +0100

    Added Mission component and Update with background image

--- src/pages/Homepage/index.js
+++ src/pages/Homepage/index.js
@@ -2,12 +2,14 @@ import React from "react";
 import Welcome from "../../components/welcome";
 import Main from "../../components/main";
 import Article from "../../components/Article";
+import Mission from "../../components/mission";
 
 export default function Homepage() {
   return (
     <>
       <Main />
       <Welcome />
+      <Mission />
       <Article />
     </>
   );
