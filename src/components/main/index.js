78f3a0d93f3dd9d5a01649452f9996650964a6f5
joshtom <olajidejoshua4real@gmail.com>
2020-04-10 15:58:10 +0100

    Update main page styling

--- src/components/main/index.js
+++ src/components/main/index.js
@@ -14,7 +14,7 @@ export default function Main() {
                 <div className="row">
                 <div className="col-lg-6 col-md-6 col-sm-12">
                     <h1 className="display-4"> BE INSPIRED </h1>
-                    <p className="col-lg-8 col-md-6 col-sm-12"> "And it came to pass, that, as the people pressed upon Him to hear the word of GOD, he stood by the lake of Gennesaret" </p>
+                    <p className="col-lg-8 col-md-6 col-sm-12 p-0"> "And it came to pass, that, as the people pressed upon Him to hear the word of GOD, he stood by the lake of Gennesaret" </p>
                     <Button variant="contained" color="default" className="mr-4 btn btn-warning"> Signup </Button>
                     <Button variant="outlined" color="default" className="btn-2"> Learn More </Button>
                 </div>
